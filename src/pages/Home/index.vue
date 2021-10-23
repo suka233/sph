83f036be526c7b59bb6cab458657129ec1968ab4
suka <suka233@outlook.com>
2021-10-23 14:42:20 +0800

    完成静态组件的拆分

--- src/pages/Home/index.vue
+++ src/pages/Home/index.vue
@@ -1,12 +1,39 @@
 <template>
 <div>
-  Home
+  <TypeNav></TypeNav>
+  <ListContainer></ListContainer>
+  <TodayRecommend></TodayRecommend>
+  <Rank></Rank>
+  <Like></Like>
+  <Floor1></Floor1>
+  <Floor2></Floor2>
+  <Brand></Brand>
 </div>
 </template>
 
 <script>
+import TypeNav from 'components/Home/TypeNav'
+import ListContainer from "components/Home/ListContainer"
+import TodayRecommend from "components/Home/TodayRecommend"
+import Rank from "components/Home/Rank"
+import Like from "components/Home/Like"
+import Floor1 from "components/Home/Floor1"
+import Floor2 from "components/Home/Floor2"
+import Brand from "components/Home/Brand"
+
+
 export default {
-  name: "Home"
+  name: "Home",
+  components:{
+    TypeNav,
+    ListContainer,
+    TodayRecommend,
+    Rank,
+    Like,
+    Floor1,
+    Floor2,
+    Brand
+  }
 }
 </script>
 
