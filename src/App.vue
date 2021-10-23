bf51d334432e103589474a1763226b014be35a71
suka <suka233@outlook.com>
2021-10-23 11:45:03 +0800

    完成header和footer拆分

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,21 @@
 <template>
   <div id="app">
+    <Header></Header>
     <router-view></router-view>
+    <Footer v-show="!$route.meta.hideFooter"></Footer>
   </div>
 </template>
 
 <script>
 
-
+import Header from 'components/Header'
+import Footer from 'components/Footer'
 export default {
-  name: 'App'
+  name: 'App',
+  components:{
+    Header,
+    Footer
+  }
 }
 </script>
 
--- src/routes/index.js
+++ src/routes/index.js
@@ -1,3 +1,12 @@
-export default [
+import Home from 'pages/Home'
+import Login from 'pages/Login'
+import Register from 'pages/Register'
+import Search from 'pages/Search'
 
+export default [
+    {path:"/Home",component:Home},
+    {path:"/Login",component:Login,meta:{hideFooter:true}},
+    {path:"/Register",component:Register,meta:{hideFooter:true}},
+    {path:"/Search",component:Search},
+    {path:"/",redirect:"/Home"}//这里我把redirect的参数写成了空,显然没理解这个配置的意思,这个配置是将path路径重定向到redirect声明的路径下
 ]
