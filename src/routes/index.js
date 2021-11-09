3fcae2cd2bcec904abafaaa77a9409313b4669e0
suka <suka233@outlook.com>
2021-11-09 17:50:16 +0800

    页面格式化,未添加其它功能

--- src/routes/index.js
+++ src/routes/index.js
@@ -4,18 +4,20 @@ import Register from 'pages/Register'
 import Search from 'pages/Search'
 
 export default [
-    {path:"/Home",component:Home},
-    {path:"/Login",component:Login,meta:{hideFooter:true}},
-    {path:"/Register",component:Register,meta:{hideFooter:true}},
-    {name:"Search",
-        path:"/Search/:keyword?",
-        component:Search,
-        props: route =>({
-            keyword:route.params.keyword,
+    {path: "/Home", component: Home},
+    {path: "/Login", component: Login, meta: {hideFooter: true}},
+    {path: "/Register", component: Register, meta: {hideFooter: true}},
+    {
+        name: "Search",
+        path: "/Search/:keyword?",
+        component: Search,
+        props: route => ({
+            keyword: route.params.keyword,
             categoryname: route.query.categoryname,
             category1id: route.query.category1id,
             category2id: route.query.category2id,
             category3id: route.query.category3id
-        })},
-    {path:"/",redirect:"/Home"}//这里我把redirect的参数写成了空,显然没理解这个配置的意思,这个配置是将path路径重定向到redirect声明的路径下
+        })
+    },
+    {path: "/", redirect: "/Home"}//这里我把redirect的参数写成了空,显然没理解这个配置的意思,这个配置是将path路径重定向到redirect声明的路径下
 ]
