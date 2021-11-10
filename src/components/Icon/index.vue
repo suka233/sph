c7df3858266f61c1dee44a059b24eb09005c2a9a
suka <suka233@outlook.com>
2021-11-10 18:26:17 +0800

    完成icon组件,(独立完成,比老师的更简洁,运用了less的混合

---
+++ src/components/Icon/index.vue
@@ -0,0 +1,56 @@
+<template>
+<div>
+<span :class="[`icon-${type}`,`icon-${size}`,`icon-${styles}`,`icon`]"></span>
+</div>
+</template>
+
+<script>
+export default {
+  name: "Icon",
+  props:{
+    type:String,//1:满减decrease 2:折扣discount 3:保价guarantee 4:票据invoice 5:特价special
+    size:Number,//图片尺寸 12:12*12 16:16*16
+    styles:String //hollow:样式1(空心) solid:样式2(实心)
+  }
+}
+</script>
+
+<style scoped lang="less">
+@import "../../common/css/mixin";
+//定义一个混合
+.icon-type(@type){
+  @name: ~"icon-@{type}";
+  &.@{name}{
+    &.icon-12{
+      width: 12px;
+      height: 12px;
+      &.icon-hollow{
+        .bg-image("@{type}_1");
+      }
+      &.icon-solid{
+        .bg-image("@{type}_3");
+      }
+    }
+    &.icon-16{
+      width: 16px;
+      height: 16px;
+      &.icon-hollow{
+        .bg-image("@{type}_2")
+      }
+      &.icon-solid{
+        .bg-image("@{type}_4")
+      }
+    }
+  }
+}
+.icon{
+  display: inline-block;
+  background-size: 100% 100%;
+  background-repeat: no-repeat;
+  .icon-type("decrease");
+  .icon-type("discount");
+  .icon-type("guarantee");
+  .icon-type("invoice");
+  .icon-type("special");
+}
+</style>
--- src/main.js
+++ src/main.js
@@ -1,6 +1,10 @@
 import Vue from 'vue'
 import App from './App.vue'
 
+//全局注册Icon组件
+import icon from "components/Icon"
+Vue.component("Icon",icon)
+
 //全局注册星星组件
 import stars from "components/Stars"
 Vue.component("Stars",stars)
